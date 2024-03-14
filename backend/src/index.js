583b489b6a328f1709aece626fc1fc465d5ad54a
IgorLima <igorlimadrum@gmail.com>
2024-03-14 09:47:12 -0300

    feat: implement api to user register

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
+const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { body, validationResult } = require("express-validator");
 
-const port = process.env.PORT || 8081
+//models
+const User = require("../models/User")
 
 const app = express();
 
+app.use(express.json());
+
+const PORT = process.env.PORT || 8081;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
@@ -22,6 +29,49 @@ const connectToDatabase = async () => {
 
 const startServer = () => {
     app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
+
+    const userValidator = [
+        body(['username', 'email', 'password'], 'Invalid does not Empty').not().isEmpty(),
+        body('username', 'The max username length is 20 characters').isLength({ max: 20 }),
+        body('email', 'Invalid email').isEmail(),
+        body('password', 'The minimum password length is 20 characters').isLength({ min: 6 }),
+    ]
+
+    app.post("/auth/register", userValidator, async (req, res) => {
+        const errors = validationResult(req);
+        if (errors.isEmpty()) {
+            const { username, email, password } = req.body;
+
+            const userExists = await User.findOne({ $or: [{ username: username }, { email: email }] });
+
+            if (userExists) { return res.status(422).json({ error: "User already exists" }) }
+
+            //create password
+            const salt = await bcrypt.genSalt(12);
+            const passwordHash = await bcrypt.hash(password, salt);
+
+            const createUser = await new User({
+                username,
+                email,
+                password: passwordHash
+            })
+
+            try {
+                await createUser.save();
+
+                return res.status(201).json({ message: "User created successfully!" })
+
+            } catch (err) {
+                console.error(err);
+                return res.status(500).json({ error: "A server error occurred! try again later", err })
+            }
+
+        }
+
+        return res.status(422).json({ errors: errors.array() });
+    });
+};
+
 const initializeApp = async () => {
     try {
         await connectToDatabase();
