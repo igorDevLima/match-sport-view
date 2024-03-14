5ef6766ce9526ac23cb08a9f8135ed60bccf3777
IgorLima <igorlimadrum@gmail.com>
2024-03-14 15:12:07 -0300

    refactor: create AuthController

---
+++ backend/src/app/controllers/AuthController.js
@@ -0,0 +1,51 @@
+import { validationResult } from "express-validator";
+import AuthRepository from "../repositories/AuthRepository.js";
+import bcrypt from "bcrypt"
+
+class AuthController {
+
+    // index() {
+
+    // }
+    // show() {
+
+    // }
+    async store(req, res) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        }
+
+        try {
+
+            const salt = await bcrypt.genSalt(12);
+            const passwordHash = await bcrypt.hash(req.body.password, salt);
+
+            const bodyUserNameAndEmail = {
+                username: req.body.username,
+                email: req.body.email,
+                password: passwordHash
+            }
+
+            const userExists = await AuthRepository.findByUserNameOrEmail(bodyUserNameAndEmail);
+
+            if (userExists) { return res.status(422).json({ error: "User already exists" }) }
+
+            const newUser = await AuthRepository.create(req.body);
+
+            return res.status(201).json({ message: "User created successfully!", user: newUser });
+        } catch (err) {
+            console.error(err);
+            return res.status(500).json({ error: "A server error occurred! Try again later", err });
+        }
+    }
+
+    // update() {
+
+    // }
+    // delete() {
+
+    // }
+}
+
+export default new AuthController;
