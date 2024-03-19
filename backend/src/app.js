4bc313bfe032f6a5142b6d0096f1d410f95876b8
IgorLima <igorlimadrum@gmail.com>
2024-03-18 21:27:46 -0300

    refactor: change to camel case

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,12 @@
 import express from "express";
 import authRouter from "./app/routes/AuthRoute.js";
 import userRouter from "./app/routes/UserRoute.js";
-import LeagueRouter from "./app/routes/LeagueRoute.js";
+import leagueRouter from "./app/routes/LeagueRoute.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use(authRouter, userRouter, LeagueRouter);
+app.use(authRouter, userRouter, leagueRouter);
 
 export default app;
