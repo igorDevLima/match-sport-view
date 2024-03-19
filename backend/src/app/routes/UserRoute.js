ee0a484896a995d206b4a4f6362a7df7f06a0da4
IgorLima <igorlimadrum@gmail.com>
2024-03-18 21:34:59 -0300

    fix: change middleware name to authMiddleware

--- backend/src/app/routes/UserRoute.js
+++ backend/src/app/routes/UserRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
 import UserController from "../controllers/UserController.js";
-import { verifyToken } from "../middlewares/middlewares/authMiddleware.js";
+import authMiddleware from "../middlewares/middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/user", verifyToken, (req, res) => UserController.index(req, res));
+router.get("/user", authMiddleware, (req, res) => UserController.index(req, res));
 
-router.get("/user/:id", verifyToken, (req, res) =>
+router.get("/user/:id", authMiddleware, (req, res) =>
   UserController.show(req, res)
 );
 
