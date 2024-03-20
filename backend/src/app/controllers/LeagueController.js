819eae2dfcb6971ea6fa72d923d5c5142265dc16
IgorLima <igorlimadrum@gmail.com>
2024-03-20 15:53:33 -0300

    refactor: change return league api's

--- backend/src/app/controllers/LeagueController.js
+++ backend/src/app/controllers/LeagueController.js
@@ -5,17 +5,28 @@ class LeagueController {
   async index(req, res) {
     const allLeagues = await LeagueRepository.findAll();
 
-    if (!allLeagues) throw new NotFoundError("Could not find any league!");
+    if (allLeagues.results === 0) throw new NotFoundError("Leagues not found!");
 
-    return res.status(200).json({ message: "Leagues found!", allLeagues });
+    return res.status(200).json({
+      message: "All leagues found!",
+      results: allLeagues.results,
+      allLeagues: allLeagues.response,
+    });
   }
 
   async show(req, res) {
     const league = await LeagueRepository.find(req.params.id);
 
-    if (league.results === 0) throw new NotFoundError("League not found!");
+    if (!league) throw new NotFoundError("League not found!");
 
-    return res.status(200).json({ message: "League found!", league });
+    return res.status(200).json({
+      message: "League found!",
+      league: {
+        ...league.league,
+        country: league.country,
+        seasons: league.seasons,
+      },
+    });
   }
 }
 
