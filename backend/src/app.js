15fec880367836c743b695f77abcfd257bb004c8
IgorLima <igorlimadrum@gmail.com>
2024-03-20 15:53:50 -0300

    feat: implement swagger

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,9 @@
 import "express-async-errors";
 import express from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" assert { type: "json" };
+
+//routes
 import authRouter from "./app/routes/AuthRoute.js";
 import userRouter from "./app/routes/UserRoute.js";
 import leagueRouter from "./app/routes/LeagueRoute.js";
@@ -10,6 +14,8 @@ import errorMiddleware from "./app/middlewares/error.js";
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(authRouter, userRouter, leagueRouter);
 
