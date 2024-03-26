6a8c131e1d55a7db1edf0ef16a8796bbc733901c
IgorLima <igorlimadrum@gmail.com>
2024-03-26 10:36:05 -0300

    feat: import express-async-errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import "express-async-errors";
 import express from "express";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "../swagger.json" assert { type: "json" };
+import "express-async-errors";
 
 //routes
 import authRouter from "./app/routes/AuthRoute.js";
