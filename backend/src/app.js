adf54d0a54db877ebd25b24cc3aca17b16755e20
IgorLima <igorlimadrum@gmail.com>
2024-04-03 09:13:12 -0300

    feat: add doc strings

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,13 @@ import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "../swagger.json" assert { type: "json" };
 import "express-async-errors";
 
-//routes
+//routes imports
 import authRouter from "./auth/Auth.js";
 import userRouter from "./user/User.js";
 import leagueRouter from "./football/league/League.js";
 import teamRouter from "./football/team/Team.js";
 
-//middlewares
+//middlewares imports
 import errorMiddleware from "./common/middlewares/error.js";
 import { headerConfig } from "./common/middlewares/header.js";
 import { limitRequestWithIp } from "./common/middlewares/rateLimit.js";
@@ -20,12 +20,15 @@ app.use(express.json());
 
 app.disable("x-powered-by");
 
+// Combine middleware functions
 const middlewares = [headerConfig, limitRequestWithIp("all", 7)];
 
 app.use(middlewares);
 
+// Docs route
 app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
+// Routes
 app.use("/auth", authRouter);
 
 app.use("/user", userRouter);
@@ -34,6 +37,7 @@ app.use("/football/league", leagueRouter);
 
 app.use("/football/team", teamRouter);
 
+// Error handler
 app.use(errorMiddleware);
 
 export default app;
